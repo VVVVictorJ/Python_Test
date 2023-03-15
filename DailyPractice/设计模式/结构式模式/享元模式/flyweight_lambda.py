46a3c9d1f4b1822f0e877665f2dbd0fe0c9d3de1
VVVVictorJ <VictorRobin1097@gmail.com>
2023-03-15 19:19:58 +0800

    修复lambda版享元模式

--- DailyPractice/设计模式/结构式模式/享元模式/flyweight_lambda.py
+++ DailyPractice/设计模式/结构式模式/享元模式/flyweight_lambda.py
@@ -1,29 +1,21 @@
 metaflyweight = lambda name, parents, attrs: type(
     name, parents,
-    dict(list(attrs.items()) +
-            [
-                ('__instances', dict()),
-                ('__new__',
-                    classmethod(lambda cls, *args, **kwargs:
-                     cls.__instances.setdefault
-                        (
-                            (args, tuple(kwargs.items())), cls(args, kwargs)
-                        )
-                    )
-                )
-            ]
-        )
-    )
+    dict(
+        list(attrs.items()) +
+        [('__instances', dict()),
+         ('__new__',
+          classmethod(lambda cls, *args, **kwargs: cls.__instances.setdefault(
+              tuple(args),
+              super(type(cls), cls).__new__(cls))))]))
 
 print(metaflyweight)
 
 
-class Spam(object):
+class Spam(object,metaclass=metaflyweight):
 
     def __init__(self, a, b):
         self.a = a
         self.b = b
-        
 
 
 class Egg(object):
@@ -34,15 +26,13 @@ class Egg(object):
         self.y = y
 
 
-# assert Spam(1, 2) is Spam(1, 2)
+assert Spam(1, 2) is Spam(1, 2)
 # assert Egg('a', 'b') is Egg('a', 'b')
 # assert Spam(1, 2) is not Egg(1, 2)
 
 spam1 = Spam(1, 2)
 spam2 = Spam(1, 2)
 
-print(spam1.__dict__)
-print(spam2.__dict__)
-# print(id(spam2))
-
-
+print(id(spam1))
+print(id(spam2))
+print(spam1 is spam2)
