181072d40eea6c520976d54d6b86b77150c9e5e3
VVVVictorJ <VictorRobin1097@gmail.com>
2023-03-26 22:32:45 +0800

    连接数据库操作测试

---
+++ shitwork/module/writeToDb.py
@@ -0,0 +1,21 @@
+import psycopg2
+
+conn = psycopg2.connect(database="postgres",
+                        user="postgres",
+                        password="123456",
+                        host="127.0.0.1",
+                        port="5432")
+
+print('postgreSQL数据库"postgres"连接成功!')
+
+cursor = conn.cursor()
+
+cursor.execute("SELECT deptid, deptname, createtime FROM department")    
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row[0], row[1], row[2])
+
+conn.close()
+
