495293571d30a2168931684a0f9c95cf4cf46591
VVVVictorJ <VictorRobin1097@gmail.com>
2023-03-26 22:29:08 +0800

    目前完成对docx的表格数据提取及图片提取，需要完善两个零散数据的提取。记录已处理的临时dir。

---
+++ shitwork/module/analysisDocx.py
@@ -0,0 +1,118 @@
+from docx import Document
+from lxml import etree
+from recordFileList import RecordDirList
+import os, re
+import uuid
+import zipfile
+import docx2txt
+
+#TODO 将其实现成单例
+class AnalysisDocx:
+
+    def __init__(self, filepath: str):
+        if not os.path.isfile(filepath):
+            raise TypeError("NOT FILEPATH")
+        if not filepath.endswith('.docx'):
+            raise TypeError("NOT DOCX FILE")
+        self.filepath = filepath
+        self.uuidStr = str(uuid.uuid4())
+        self.resultpath = ""
+        self.docxWordInfo = {}
+        self.docxImage = []
+        self.record = RecordDirList()
+
+    def genData(self, k, v):
+        self.docxWordInfo[k] = v
+
+    def genImageRocord(self, val):
+        self.docxImage.append(val)
+
+    def genDocxTableInfo2Txt(self):
+        try:
+            file = Document(self.filepath)
+            table = file.tables[0]
+            cells = table._cells
+            cells_string = [cell.text for cell in cells]
+            cells_value = sorted(set(cells_string), key=cells_string.index)
+            self.genData(cells_value[0], cells_value[3])
+            self.genData(cells_value[1], cells_value[4])
+            self.genData(cells_value[2], cells_value[5])
+            self.genData(cells_value[6], cells_value[10])
+            self.genData(cells_value[7], cells_value[11].replace('\n', ' '))
+            self.genData(cells_value[8], cells_value[12])
+            self.genData(cells_value[9], cells_value[13])
+            self.genData(cells_value[14], cells_value[17])
+            self.genData(cells_value[15], cells_value[18])
+            self.genData(cells_value[16], cells_value[11].replace('\n', ' '))
+            self.genData(cells_value[15], cells_value[18])
+            self.genData(cells_value[21][0:4], cells_value[21][-1])
+            self.genData(cells_value[22][0:4], cells_value[22][6:])
+
+            def isR(l: list):
+                for i in l:
+                    if '√' in i:
+                        return i
+
+            x = lambda x: isR(cells_value)
+            self.genData(cells_value[23], x(cells_value)[-4:])
+            self.genData(cells_value[28], cells_value[29].replace('\n', ' '))
+        except:
+            pass
+        try:
+            txtFileName = "{}/{}{}".format(self.resultpath, self.uuidStr, '.txt')
+            with open(txtFileName, "wb") as f:
+                for k, v in self.docxWordInfo.items():
+                    f.write("{0:{1}<10}\t{1:^10}@{2}\t{3:^10}\n".format(k, chr(12288), v, chr(12288)).encode())
+        except FileNotFoundError:
+            print('无法打开指定的文件!')
+        except LookupError:
+            print('指定了未知的编码!')
+        except UnicodeDecodeError:
+            print('读取文件时解码错误!')
+
+    def genDocxImage(self):
+        self.uuidStr = str(uuid.uuid4())
+        self.resultpath = os.path.join(os.getcwd(), self.uuidStr)
+        try:
+            doc = Document(self.filepath)
+            dict_rel = doc.part._rels
+            for rel in dict_rel:
+                rel = dict_rel[rel]
+                if "image" in rel.target_ref:
+                    if not os.path.exists(self.resultpath):
+                        os.makedirs(self.resultpath)
+                    img_name = re.findall("/(.*)", rel.target_ref)[0]
+                    word_name = os.path.splitext(self.filepath)[0]
+                    if os.sep in word_name:
+                        new_name = word_name.split('\\')[-1]
+                    else:
+                        new_name = word_name.split('/')[-1]
+                    img_name = "{}-{}{}".format(f'{new_name}',
+                                                str(uuid.uuid4()),
+                                                os.path.splitext(img_name)[-1])
+                    with open(f'{self.resultpath}/{img_name}', "wb") as f:
+                        f.write(rel.target_part.blob)
+                        self.genImageRocord(f'{self.resultpath}/{img_name}')
+        except:
+            pass
+
+    def showData(self):
+        for k, v in self.docxWordInfo.items():
+            print("{0:{1}<10}\t{1:^10}".format(k, chr(12288)),
+                  "{0}\t{1:^10}".format(v, chr(12288)))
+        for k, v in enumerate(self.docxImage):
+            print("{0:{1}<2}\t{1:^3}".format(k, chr(12288)),
+                  "{0}\t{1:^10}".format(v, chr(12288)))
+
+
+if __name__ == '__main__':
+    obj = AnalysisDocx(filepath="gc.docx")
+    # 有待封装，将其封装成一个类Producer
+    try:
+        obj.genDocxImage()
+        obj.genDocxTableInfo2Txt()
+        obj.showData()
+    except:
+        pass
+    finally:
+        obj.record.addList(obj.resultpath)
