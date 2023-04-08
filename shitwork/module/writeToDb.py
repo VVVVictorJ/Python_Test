95016a8ad88f13db5c4a2b9defdd84ccaf0c3759
VVVVictorJ <Victorrobin1097@gmail.com>
2023-04-08 23:55:12 +0800

    Arch Linux connect pg

--- shitwork/module/writeToDb.py
+++ shitwork/module/writeToDb.py
@@ -1,21 +1,26 @@
 import psycopg2
 
-conn = psycopg2.connect(database="postgres",
-                        user="postgres",
+#conn = psycopg2.connect(database="postgres",
+#                        user="postgres",
+#                        password="123456",
+#                        host="127.0.0.1",
+#                        port="5432")
+conn = psycopg2.connect(database="myDatabase",
+                        user="vic",
                         password="123456",
-                        host="127.0.0.1",
+                        host="127.0.0.1",   
                         port="5432")
 
 print('postgreSQL数据库"postgres"连接成功!')
 
 cursor = conn.cursor()
 
-cursor.execute("SELECT deptid, deptname, createtime FROM department")    
+#cursor.execute("SELECT deptid, deptname, createtime FROM department")    
 
-rows = cursor.fetchall()
+#rows = cursor.fetchall()
 
-for row in rows:
-    print(row[0], row[1], row[2])
+#for row in rows:
+#    print(row[0], row[1], row[2])
 
 conn.close()
 
