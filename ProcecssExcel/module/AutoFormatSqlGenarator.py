9a1d242216d9222e0049e4a32d9c24546e07fa82
VVVVictorJ <956951190@qq.com>
2023-05-09 16:34:13 +0800

    持续优化处理excel模块

--- ProcecssExcel/module/AutoFormatSqlGenarator.py
+++ ProcecssExcel/module/AutoFormatSqlGenarator.py
@@ -2,5 +2,26 @@ import logging
 
 
 class AutoFormatSqlGenarator:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, FormatTemplateStr: str) -> None:
+        self.FormatTemplateStr = FormatTemplateStr
+
+    def GenFormatString(self, param) -> str:
+        for k, v in param:
+            print(self.FormatTemplateStr.format())
+        return
+
+    @staticmethod
+    def Test():
+        exec('print("Hello")')
+
+
+if __name__ == "__main__":
+    AutoFormatSqlGenarator.Test()
+    table = {"Sjoerd": 4127, "Jack": 4098, "Dcab": 8637678}
+    print(
+        "Jack: {0[Jack]:d}; Sjoerd: {0[Sjoerd]:d}; " "Dcab: {0[Dcab]:d}".format(table)
+    )
+    tmp = {"a": 123}
+    print(
+        "1:{0[a]:d}".format(tmp)
+    )
