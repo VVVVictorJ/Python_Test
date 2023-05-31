59f3d8471abac97fe1ffc3d0fa8e39999937e7b6
VVVVictorJ <956951190@qq.com>
2023-05-31 08:51:41 +0800

    输出格式化字符串至文件

--- ProcessExcel/module/ProcessToFile.py
+++ ProcessExcel/module/ProcessToFile.py
@@ -1,12 +1,36 @@
 import logging
 import os
+from functools import wraps
+
+logging.basicConfig(
+    filename="example.log",
+    encoding="utf-8",
+    level=logging.DEBUG,
+)
+
+
+def log(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logging.debug("Function {} is running.".format(func.__name__))
+        result = func(*args, **kwargs)
+        logging.debug("Function {} is finising.\n".format(func.__name__))
+        return result
+
+    return wrapper
 
 
 class BackUpToFile:
     def __init__(self) -> None:
-        pass
+        self.resultPath = os.path.sep.join([os.getcwd(), "test.txt"])
+        # print(self.resultPath)
 
-    # 协程处理
-    def ProcessListToFile(self, ):
-        pass
+    @log
+    def ProcessDictToFile(self, param: dict, templateString: str):
+        with open(self.resultPath, "w+") as f:
+            for k, v in param.items():
+                f.write(templateString.format(v) + "\n")
 
+
+# obj = BackUpToFile()
+# obj.ProcessListToFile()
