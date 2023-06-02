bdea7e742e7b1887fb4066075addddb25c793cb5
VVVVictorJ <956951190@qq.com>
2023-06-02 16:25:44 +0800

    学习对象形式联表，class形式查询

--- SqlAlchemyLearn/端侧测试/ormJoinClassLearn.py
+++ SqlAlchemyLearn/端侧测试/ormJoinClassLearn.py
@@ -11,18 +11,12 @@ from sqlalchemy import (
     and_,
     bindparam,
     create_engine,
+    func,
     or_,
     select,
 )
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
-"""
-SqlAlchemy class对象式查询
-Select 
-and 
-or
-filter_by
-"""
 
 class Base(DeclarativeBase):
     pass
@@ -57,44 +51,15 @@ engine = create_engine(
     "mysql+pymysql://admin:zhj123456@127.0.0.1:3306/test?charset=utf8"
 )
 
-# stmt = select(Pytest).where(Pytest.username == "victor")
-# print(stmt)
 
-# with engine.connect() as conn:
-#     # for row in conn.execute(stmt):
-#     #     print(row)
-#     # row = conn.execute(select(Pytest)).first()
-#     # row = conn.scalars(select(Pytest)).first()
-
-#     rows = conn.execute(
-#         select(Pytest.username, Address)
-#         .where(Pytest.id == Address.user_id)
-#         .order_by(Address.email_address)
-#     ).all()
-
-#     # row = conn.scalars(
-#     #     select(Pytest.username)
-#     #     .where(Pytest.id == Address.id)
-#     #     .order_by(Address.email_address)
-#     # )
-#     for row in rows:
-#         print(row)
-
-# 自动作全连接
-stmt = select(Address.email_address).where(
-    and_(
-        or_(Pytest.username == "victor", Pytest.name == "sandy"),
-        Address.user_id == Pytest.id,
-    )
+stmt = (
+    select(Pytest.name, func.count(Address.id).label("count"))
+    .join(Address)
+    .group_by(Pytest.name)
+    .having(func.count(Address.id) > 1)
 )
-
-stmt1 = select(Pytest).filter_by(username="victor")
+print(stmt)
 
 with engine.connect() as conn:
-    rows = conn.execute(stmt)
-    for row in rows:
-        print(row)
-
-    rows = conn.execute(stmt1)
-    for row in rows:
-        print(row)
+    result = conn.execute(stmt)
+    print(result.all())
---
+++ SqlAlchemyLearn/端侧测试/ormJoinLearn.py
@@ -0,0 +1,84 @@
+from typing import List, Optional
+
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Insert,
+    Integer,
+    MetaData,
+    String,
+    Table,
+    and_,
+    bindparam,
+    create_engine,
+    func,
+    or_,
+    select,
+)
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+"""
+SqlAlchemy 对象式查询
+Join 练习
+"""
+
+metadata_obj = MetaData()
+
+pytest = Table(
+    "py_test",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("username", String(30)),
+    Column("name", String(100)),
+)
+
+address_table = Table(
+    "address",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    # 此处需注意是py_test.id, 真实表名.字段名
+    Column("user_id", ForeignKey("py_test.id"), nullable=False),
+    Column("username", String(20)),
+    Column("email_address", String, nullable=False),
+)
+
+print(select(pytest.c.name))
+
+# 自动全连接
+print(select(pytest.c.name, address_table.c.email_address))
+
+print(
+    select(pytest.c.name, address_table.c.email_address).join_from(
+        pytest, address_table
+    )
+)
+
+# 仅指示JOIN的右侧，推断左侧
+print(select(pytest.c.name, address_table.c.email_address).join(address_table))
+
+print(select(func.count("*")).select_from(pytest))
+
+# 连接
+print(
+    select(address_table.c.email_address)
+    .select_from(pytest)
+    .join(address_table, pytest.c.id == address_table.c.user_id)
+)
+
+# outer join
+print(select(pytest).join(address_table, isouter=True))
+
+# full join
+print(select(pytest).join(address_table, full=True))
+
+"""
+order by
+group by 
+having
+"""
+
+print(select(pytest).order_by(pytest.c.username))
+
+# 升/降序
+print(select(pytest).order_by(pytest.c.username.desc()))
+
