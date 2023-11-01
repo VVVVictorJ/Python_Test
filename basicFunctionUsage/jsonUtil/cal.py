9680e7a0ce6afc5119b0b0f7d7a89b338247d1d8
VVVVictorJ <956951190@qq.com>
2023-11-01 14:26:21 +0800

    月薪计算税值

--- basicFunctionUsage/jsonUtil/cal.py
+++ basicFunctionUsage/jsonUtil/cal.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from pandas import DataFrame, Series
 
 warnings.filterwarnings(action="once")
 
@@ -39,6 +40,16 @@ boundary = {
     "5": [420000, 660000],
     "6": [660000, 960000],
     "7": [960000],
+}
+
+yearAward_tax_rate_and_deduction = {
+    "1": [0.03, 0],
+    "2": [0.10, 210],
+    "3": [0.20, 1410],
+    "4": [0.25, 2660],
+    "5": [0.30, 4410],
+    "6": [0.35, 7160],
+    "7": [0.45, 15160],
 }
 
 
@@ -131,24 +142,47 @@ def GetTax(Sbonus, flag):
             )
 
 
+def getTaxYearAward(Sbonus):
+    if SplitPart := Sbonus / 12 < 3000:
+        return (
+            yearAward_tax_rate_and_deduction.get("1")[0],
+            yearAward_tax_rate_and_deduction.get("1")[1],
+        )
+    elif SplitPart := Sbonus / 12 >= 3000 and SplitPart < 12000:
+        return (
+            yearAward_tax_rate_and_deduction.get("2")[0],
+            yearAward_tax_rate_and_deduction.get("2")[1],
+        )
+    elif SplitPart := Sbonus / 12 >= 1200 and SplitPart < 25000:
+        return (
+            yearAward_tax_rate_and_deduction.get("3")[0],
+            yearAward_tax_rate_and_deduction.get("3")[1],
+        )
+    elif SplitPart := Sbonus / 12 >= 25000 and SplitPart < 35000:
+        return (
+            yearAward_tax_rate_and_deduction.get("4")[0],
+            yearAward_tax_rate_and_deduction.get("4")[1],
+        )
+    elif SplitPart := Sbonus / 12 >= 35000 and SplitPart < 55000:
+        return (
+            yearAward_tax_rate_and_deduction.get("5")[0],
+            yearAward_tax_rate_and_deduction.get("5")[1],
+        )
+    elif SplitPart := Sbonus / 12 >= 55000 and SplitPart < 80000:
+        return (
+            yearAward_tax_rate_and_deduction.get("6")[0],
+            yearAward_tax_rate_and_deduction.get("6")[1],
+        )
+    elif SplitPart := Sbonus / 12 >= 80000:
+        return (
+            yearAward_tax_rate_and_deduction.get("7")[0],
+            yearAward_tax_rate_and_deduction.get("7")[1],
+        )
+
+
 def common_cal(MonthSalary):
-    return MonthSalary * 12 - 60000 - 1000 * 12 - 2000 * 12
-
-
-def MergeCal(MonthSalary, YearEndAwards):
-    if MonthSalary == 0:
-        return "Invaild Salary"
-
-    if sumYearIncome := MonthSalary * 12 <= 60000:
-        return "SumYearIncome is less than 60000"
-
-    taxRate, deduction, _ = GetTax(MonthSalary, 1)
-    # print(taxRate, deduction, _)
-    Personal_Income_Tax = (
-        common_cal(MonthSalary) + YearEndAwards
-    ) * taxRate - deduction
-
-    return Personal_Income_Tax
+    # return MonthSalary * 12 - 60000 - 1000 * 12 - 2000 * 12
+    return MonthSalary * 12 - 60000
 
 
 def SeperateCal(MonthSalary, YearEndAwards):
@@ -159,11 +193,26 @@ def SeperateCal(MonthSalary, YearEndAwards):
         return "SumYearIncome is less than 60000"
 
     taxRateMonth, deductionMonth, _ = GetTax(MonthSalary, 1)
-    taxRateYear, deductionYear, _ = GetTax(YearEndAwards, 0)
+    taxRateYear, deductionYear = getTaxYearAward(YearEndAwards)
     Personal_Income_Tax = common_cal(MonthSalary) * taxRateMonth - deductionMonth
     YearEndAwards_Income_Tax = YearEndAwards * taxRateYear - deductionYear
 
     return Personal_Income_Tax + YearEndAwards_Income_Tax
+
+
+def MergeCal(MonthSalary, YearEndAwards):
+    if MonthSalary == 0:
+        return "Invaild Salary"
+
+    if sumYearIncome := MonthSalary * 12 <= 60000:
+        return "SumYearIncome is less than 60000"
+
+    taxRate, deduction, _ = GetTax(MonthSalary, 1)
+    a = common_cal(MonthSalary)
+    # print(taxRate, deduction, _)
+    Personal_Income_Tax = (a + YearEndAwards) * taxRate - deduction
+
+    return Personal_Income_Tax
 
 
 def OptimizeCal(MonthSalary, YearEndAwards):
@@ -189,8 +238,33 @@ def OptimizeCal(MonthSalary, YearEndAwards):
 
 # 目前收入未知
 
+print("Seperate Calculate: {}".format(SeperateCal(12000, 1000000)))
+print("Merge Calculate: {}".format(MergeCal(12000, 1000000)))
+print("Optimize Calculate: {}".format(OptimizeCal(12000, 1000000)))
+# print([i for i in range(0, 130001, 1000)] * 3)
+# print([SeperateCal(12000,i)for i in range(0, 130001, 1000)]+ [MergeCal(12000,i)for i in range(0, 130001, 1000)] + [OptimizeCal(12000,i)for i in range(0, 130001, 1000)])
+# print([MergeCal(12000,i)for i in range(0, 130001, 1000)])
+# print([OptimizeCal(12000,i)for i in range(0, 130001, 1000)])
 
-print("Merge Calculate: {}".format(MergeCal(12000, 40000)))
-# # print("Merge Calculate: {}".format(MergeCal(0, 40000)))
-print("Seperate Calculate: {}".format(SeperateCal(12000, 40000)))
-print("Optimize Calculate: {}".format(OptimizeCal(12000, 40000)))
+# # # print("Merge Calculate: {}".format(MergeCal(0, 40000)))
+# sns.set()
+
+# data = {
+#     # "salary": [12000, 12000, 12000, 12000, 12000, 12000, 12000, 12000, 12000],
+#     "yearAwards": [i for i in range(0, 130001, 1000)] * 3,
+#     "tax": [SeperateCal(12000, i) for i in range(0, 130001, 1000)]
+#     + [MergeCal(12000, i) for i in range(0, 130001, 1000)]
+#     + [OptimizeCal(12000, i) for i in range(0, 130001, 1000)],
+#     "property": ["Sep" for i in range(0, 130001, 1000)]+["Merge" for i in range(0, 130001, 1000)]+["Optimize" for i in range(0, 130001, 1000)],
+# }
+# # data = {
+
+# # }
+# df = DataFrame(data)
+# print(df)
+# fig, ax = plt.subplots(figsize=(16, 10), dpi=80)
+# # sns.stripplot(x=df.sg, y=df.hwy, jitter=0.25, size=8, ax=ax, linewidth=0.5)
+# sns.swarmplot(
+#     x="yearAwards", y="tax", data=df, hue="property", dodge=False, palette="husl"
+# )
+# plt.savefig("1.png")
